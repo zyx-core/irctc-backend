59bbd81ac6f609eea99774a0ba997faac5f004f9
zyx-core <ershadpersonal123@gmail.com>
2026-06-09 10:27:41 +0530

    Delete Backend directory

--- Backend/Models/Station.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.Models
-{
-    public class Station
-    {
-        public int Id { get; set; }
-        public required string Code { get; set; }
-        public required string Name { get; set; }
-    }
-}
--- Backend/Services/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Backend.Services
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string toName, string subject, string htmlContent);
-    }
-}
